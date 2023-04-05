c0152cf55b610134b99c00a3d7189b7e245b5a9f
Pryyyyyyyyyyyy <anna27pr@gmail.com>
2023-04-05 10:45:17 +0300

    1111

---
+++ src/main/java/ClassA.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClassA {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.guinnessworldrecords.com/");
+    }
+}

545f7933d6635ffeda12efb20f0f7b5fff70ce63
Pryyyyyyyyyyyy <anna27pr@gmail.com>
2023-04-22 15:56:22 +0300

    Merge remote-tracking branch 'github/master'

--- homework-27/src/main/java/org/example/TestScenario1.java
+++ homework-27/src/main/java/org/example/TestScenario1.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.time.Duration;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -25,33 +28,27 @@ public class TestScenario1 {
     }
 
     @Test
-    public void testScenario1() throws InterruptedException {
+    public void testScenario1() {
         driver.get("https://dan-it.com.ua/uk/");
-        WebElement adultsLink = driver.findElement(By.xpath("//a[contains(text(),'Для дорослих')]"));
-        Actions actions = new Actions(driver);
-        actions.moveToElement(adultsLink).build().perform();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        String parentWindowHandle = driver.getWindowHandle();
+        // Открываем первое окно в фоновом режиме
+        String parentWindowHandle1 = driver.getWindowHandle();
+        String link = driver.findElement(By.xpath("//a[contains(@href, '/front-end/') and contains(text(),'FrontEnd')]")).getAttribute("href");
+        ((JavascriptExecutor) driver).executeScript("window.open('" + link + "', '_blank');");
 
-        WebElement frontEndLink = driver.findElement(By.xpath("//a[contains(@href, '/front-end/') and contains(text(),'FrontEnd')]"));
-        Actions actions1 = new Actions(driver);
-        actions.moveToElement(frontEndLink).build().perform();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // Переключаемся на главное окно и открываем второе окно в фоновом режиме
+        driver.switchTo().window(parentWindowHandle1);
+        String parentWindowHandle2 = driver.getWindowHandle();
+        String link2 = driver.findElement(By.xpath("//a[contains(@href, '/java/') and contains(text(),'Java')]")).getAttribute("href");
+        ((JavascriptExecutor) driver).executeScript("window.open('" + link2 + "', '_blank');");
 
-// Открываем найденный элемент в новом окне браузера в фоне
-        String link = frontEndLink.getAttribute("href");
-        String script = "window.open('" + link + "', '_blank');";
-        ((JavascriptExecutor) driver).executeScript(script);
+        // Переключаемся на главное окно и открываем третье окно в фоновом режиме из второго окна
+        driver.switchTo().window(parentWindowHandle2);
+        String link3 = driver.findElement(By.xpath("//a[contains(@href, '/qa/') and contains(text(),'Quality Assurance (QA)')]")).getAttribute("href");
+        ((JavascriptExecutor) driver).executeScript("window.open('" + link3 + "', '_blank');");
 
-
-        WebElement javaLink = driver.findElement(By.xpath("//a[contains(text(),'Java')]"));
-        javaLink.click();
-
-        WebElement qaLink = driver.findElement(By.xpath("//a[contains(text(),'Quality Assurance (QA)')]"));
-        qaLink.click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
+        // Переключаемся на главное окно и проверяем, что количество окон равно 3
+        driver.switchTo().window(parentWindowHandle1);
         Set<String> windowHandles = driver.getWindowHandles();
         int numberOfWindows = windowHandles.size();
         Assert.assertEquals(numberOfWindows, 4, "Number of windows opened is not 4");

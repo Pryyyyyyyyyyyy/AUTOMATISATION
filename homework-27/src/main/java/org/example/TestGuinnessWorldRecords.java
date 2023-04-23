9979443249a1a7bdbf3ab236b6c3851c85502b60
Pryyyyyyyyyyyy <anna27pr@gmail.com>
2023-04-23 19:34:10 +0300

    Merge remote-tracking branch 'github/master'

--- homework-27/src/main/java/org/example/TestGuinnessWorldRecords.java
+++ homework-27/src/main/java/org/example/TestGuinnessWorldRecords.java
@@ -14,7 +14,7 @@ public class TestGuinnessWorldRecords {
 
     @BeforeClass
     public void setUp() {
-        // Set up Chrome driver
+
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -22,25 +22,21 @@ public class TestGuinnessWorldRecords {
 
     @Test
     public void testRememberMeCheckbox() {
-        // Navigate to login page
+
         driver.get("https://www.guinnessworldrecords.com/Account/Login");
 
-        // Find Remember Me checkbox and check if it is unchecked by default
         WebElement rememberMeCheckbox = driver.findElement(By.id("RememberMe"));
         Assert.assertFalse(rememberMeCheckbox.isSelected(), "Remember Me checkbox should be unchecked by default");
 
-        // Click Remember Me checkbox and check if it is selected
         rememberMeCheckbox.click();
         Assert.assertTrue(rememberMeCheckbox.isSelected(), "Remember Me checkbox should be selected");
 
-        // Click Remember Me checkbox again and check if it is unchecked
         rememberMeCheckbox.click();
         Assert.assertFalse(rememberMeCheckbox.isSelected(), "Remember Me checkbox should be unchecked");
     }
 
     @AfterClass
     public void tearDown() {
-        // Close the browser
         driver.quit();
     }
 

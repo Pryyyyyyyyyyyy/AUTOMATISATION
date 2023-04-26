190bfb157e7e351ffd41bf10af48ba62ee6b2eb5
Pryyyyyyyyyyyy <anna27pr@gmail.com>
2023-04-26 09:50:23 +0300

    Merge remote-tracking branch 'github/master'

--- homework-28/src/main/java/foxtrot/FoxtrotDataProvider.java
+++ homework-28/src/main/java/foxtrot/FoxtrotDataProvider.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class FoxtrotDataProvider {
@@ -20,15 +21,36 @@ public class FoxtrotDataProvider {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.foxtrot.com.ua/");
+
         WebElement cityElement = driver.findElement(By.xpath("//li[@class='popup__cities_item']//a[contains(text(),'Ужгород')]"));
         cityElement.click();
     }
     @Test(dataProvider = "searchWords")
-    public void searchTest(String searchWord, String newWord) {
-        WebElement searchField = driver.findElement(By.cssSelector("input.evinent-search-input"));
+    public void searchTest(String searchWord) {
+        Waiters waiters = new Waiters(driver);
+     /*   WebElement searchField = driver.findElement(By.cssSelector("input.evinent-search-input"));*/
+        WebElement searchField = waiters.waitForVisabilityOfElementReturn(By.xpath("//input[@placeholder='Я шукаю ...']"));
         searchField.click();
+        searchField.sendKeys(searchWord);
+        WebElement button = waiters.waitForVisabilityOfElementReturn(By.xpath(" //input[@value='Знайти']"));
+        button.click();
 
-        WebElement input = (new Waiters(driver))
+
+        waiters.waitForTitleContains("Знайдено по запиту");
+        if((driver.findElement(By.tagName("h1")).getText()).contains("Результати пошуку")){
+            waiters.waitForVisabilityOfElement(By.xpath("//div[@class='search-page__box-title']/label"));
+            assertEquals((driver.findElement(By.xpath("//div[@class='search-page__box-title']/label")).getText()),"«"+searchWord+"»",
+                    "Actual result = "+ (driver.findElement(By.xpath("//div[@class='search-page__box-title']/label")).getText())+
+                            " Expected "+ searchWord);
+        }else {
+            waiters.waitForVisabilityOfElement(By.tagName("h1"));
+            String result = driver.findElement(By.tagName("h1")).getText().replace("Знайдено по запиту ","");
+            assertEquals(result,"«"+searchWord+"»",
+                    "Actual result = "+ (driver.findElement(By.tagName("h1")).getText())+
+                            " Expected "+searchWord);
+        }
+        driver.quit();
+   /*     WebElement input = (new Waiters(driver))
                 .waitForPresenceOfElementLocated(By.xpath("//input[@placeholder='Я шукаю ...']"));
         input.sendKeys(searchWord);
         input.submit();
@@ -37,14 +59,15 @@ public class FoxtrotDataProvider {
         assertTrue(driver.findElement(By.cssSelector("div.page__title")).getText()
                 .replace("Знайдено по запиту", "").equalsIgnoreCase(newWord));
         driver.quit();
-
+*/
     }
 
     @DataProvider(name = "searchWords")
     public Object[][] searchData() {
         return new Object[][]{
                 {"машина"},
-                {"input"}
+                {"input"},
+                {"смысл"}
         };
     }
 }
